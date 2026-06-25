ec903ad66c96a9f5c41dd4fc059c6cd8813a1e97
dragoncentral6 <dragon6.central@gmail.com>
2026-06-25 06:12:25 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getFirestore,initializeFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA9iDdqFnLFACBg3zH_BTy1ocIEahibVjs",
